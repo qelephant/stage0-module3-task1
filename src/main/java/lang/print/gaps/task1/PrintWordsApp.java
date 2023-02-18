ec003b01e9fa5cd3fc49474041548ac2ce1df369
qyerke <70740293+qelephant@users.noreply.github.com>
2023-02-18 23:53:42 +0600

    Update PrintWordsApp.java

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.print("java is smth");
+        System.out.println("java is smth");
     }
 }
